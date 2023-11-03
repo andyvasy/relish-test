193b117595f3472a732f8c04e93e95087caa393f
Andrey <andyvasyalp@gmail.com>
2023-11-03 14:56:20 -0500

    client build target

--- photo-library-ui/vite.config.js
+++ photo-library-ui/vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "../photo-library/static",
+  },
   server: {
     proxy: {
       "/externalapi": {
